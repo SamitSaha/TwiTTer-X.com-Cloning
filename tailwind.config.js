6048bb1bb7af28a583541fa3e8d5074b107f1c8c
Samit Saha <72096509+SamitSaha@users.noreply.github.com>
2024-07-01 18:10:51 +0600

    The twitter home page cloning has been done.
    Tailwind css and html is used to design this home page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      textColor: '#202327',
+    },
+    inset: {
+     '5': '5px',
+     '8': '8px',
+     'inherit': 'inherit',
+     '0': '0',
+     'x': 'x',
+     'y': 'y'
+   },
+  },
+  plugins: [],
+}
